2c3e9a467a4d3c9d444fb21c6e79370150bfcfe5
Huzaifa <HuzaifaIslamRakib@gmail.com>
2024-09-24 03:01:19 +0600

    made history from noakhali donnation section

--- features.js
+++ features.js
@@ -32,3 +32,49 @@ function selectedBtn(id) {
   );
   getById(id).classList.add("bg-btn1", "text-xl", "font-semibold", "text-pr");
 }
+
+function isNum(value) {
+  if (isNaN(value) || value == "") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function inputToNum(id) {
+  const value = getById(id).value;
+  if (isNum(value)) {
+    let temp = parseFloat(value);
+    if (temp <= 0) {
+      alert("the input amount is invalid");
+    } else {
+      return temp;
+    }
+  } else {
+    alert("the input amount is invalid");
+    return false;
+  }
+}
+
+function makeHistory(amount, msg) {
+  const date = new Date();
+  const totalLi = document.createElement("li");
+  totalLi.classList.add(
+    "space-y-4",
+    "rounded-2xl",
+    "border",
+    "border-solid",
+    "border-[#1111111A]",
+    "p-8",
+  );
+  const el1 = document.createElement("p");
+  el1.innerText = `${amount} ${msg}`;
+  el1.classList.add("text-xl", "font-bold", "text-pr");
+  const el2 = document.createElement("p");
+  el2.innerText = `${date}`;
+  el2.classList.add("font-light", "text-second");
+  totalLi.appendChild(el1);
+  totalLi.appendChild(el2);
+  getById("listOfHistory").appendChild(totalLi);
+  console.log(totalLi);
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // default
 selectedBtn("donate");
 normalBtn("history");
+let mainBalance = parseFloat(getById("mainBalance").innerText);
 
 // making history button work
 function showHistory() {
@@ -19,5 +20,22 @@ function showDonation() {
   getById("historyPage").classList.add("hidden");
   getById("donatePage").classList.remove("hidden");
 }
-
 getById("donate").addEventListener("click", showDonation);
+
+// noakhali donation section
+function donateNoakhali() {
+  const donate = inputToNum("noakhaliInput");
+  getById("noakhaliInput").value = "";
+  if (donate === false) {
+    return;
+  } else if (donate > mainBalance) {
+    alert("Invalid input");
+  } else {
+    getById("givenNoakhali").innerHTML = `${donate} BDT`;
+    mainBalance -= donate;
+    getById("mainBalance").innerHTML = mainBalance;
+    makeHistory(donate, "Taka is Donated for famine-2024 at Feni, Bangladesh");
+  }
+}
+
+getById("donateNoakhali").addEventListener("click", donateNoakhali);
